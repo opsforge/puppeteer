dad35aa07f5f7ae389bc63a8a809e0c47da95a59
George Svachulay <opsforge-n7>
2017-02-10 23:56:54 +0000

    Minor syntax and bugfixes

--- puppetdb/docker-entrypoint.sh
+++ puppetdb/docker-entrypoint.sh
@@ -5,8 +5,9 @@ if [ ! -d "/etc/puppetlabs/puppetdb/ssl" ]; then
     sleep 1
   done
   set -e
-  /opt/puppetlabs/bin/puppet agent --verbose --onetime --no-daemonize --certname puppetdb --waitforcert 120
+  /opt/puppetlabs/bin/puppet agent --verbose --onetime --no-daemonize --waitforcert 120
   /opt/puppetlabs/server/bin/puppetdb ssl-setup -f
+  /opt/puppetlabs/bin/puppet agent --verbose --onetime --no-daemonize --certname puppetdb
 fi
 
 exec /opt/puppetlabs/server/bin/puppetdb "$@"
