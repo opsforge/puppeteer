33376e7bc6954b536451be29674d997c40a5ec3c
George Svachulay <opsforge-n7>
2017-02-11 20:07:38 +0000

    Minor syntax and bugfixes

--- puppetserver/docker-entrypoint.sh
+++ puppetserver/docker-entrypoint.sh
@@ -11,4 +11,7 @@ if test -n "${PUPPETDB_SERVER_URLS}" ; then
   sed -i "s@^server_urls.*@server_urls = ${PUPPETDB_SERVER_URLS}@" /etc/puppetlabs/puppet/puppetdb.conf
 fi
 
+# Autosing DNS ALT NAME Cert 5 min after prov. for puppetdb only - wait for the DB to submit the request
+/bin/bash -c 'sleep 5m && /opt/puppetlabs/bin/puppet cert --allow-dns-alt-names sign puppetdb'
+
 exec /opt/puppetlabs/bin/puppetserver "$@"
